d36a6ff6b9ec17aa96b9d7f2585848dd91078fd5
万里涛 <wanlitao_3055601@outlook.com>
2021-09-18 20:06:52 +0800

    add string sha256 hash extension methods

--- src/HEF.Security.Cryptography/Extensions/StringEncryptExtensions.cs
+++ src/HEF.Security.Cryptography/Extensions/StringEncryptExtensions.cs
@@ -25,7 +25,7 @@ namespace HEF.Security.Cryptography
         /// <returns></returns>
         public static string ToMD5(this string str, string encodeName)
         {
-            if (str.IsNullOrEmpty())
+            if (str.IsNullOrWhiteSpace())
                 return string.Empty;
 
             var bytes = Encoding.GetEncoding(encodeName).GetBytes(str);
@@ -48,7 +48,53 @@ namespace HEF.Security.Cryptography
                 byte[] md5Hash = md5.ComputeHash(bytes);
                 string result = BitConverter.ToString(md5Hash);
 
-                return result.Replace("-", "");
+                return result.Replace("-", string.Empty);
+            }
+        }
+        #endregion
+
+        #region SHA256
+        /// <summary>
+        /// SHA256加密 字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string ToSHA256(this string str)
+        {
+            return ToSHA256(str, "utf-8");
+        }
+        /// <summary>
+        /// SHA256加密 字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="encodeName">编码格式</param>
+        /// <returns></returns>
+        public static string ToSHA256(this string str, string encodeName)
+        {
+            if (str.IsNullOrWhiteSpace())
+                return string.Empty;
+
+            var bytes = Encoding.GetEncoding(encodeName).GetBytes(str);
+
+            return ToSHA256(bytes);
+        }
+
+        /// <summary>
+        /// SHA256加密 二进制流
+        /// </summary>
+        /// <param name="bytes">二进制流</param>
+        /// <returns></returns>
+        public static string ToSHA256(this byte[] bytes)
+        {
+            if (bytes.IsEmpty())
+                return string.Empty;
+
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] sha256Hash = sha256.ComputeHash(bytes);
+                string result = BitConverter.ToString(sha256Hash);
+
+                return result.Replace("-", string.Empty);
             }
         }
         #endregion
